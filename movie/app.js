811aab0af1db912481384f773a7c5c9a1a2e2165
vivien <[1870495688@qq.com](mailto:1870495688@qq.com)>
2018-10-27 21:31:20 +0800

    detail-bgMusic

--- movie/app.js
+++ movie/app.js
@@ -1,4 +1,7 @@
 //app.js
 App({
- 
+ globalData: {
+   g_isPlayingMusic: false,
+   g_currentMusicPostId: null
+ }
 })
--- movie/pages/news/news-detail/news-detail.js
+++ movie/pages/news/news-detail/news-detail.js
@@ -1,12 +1,13 @@
 // pages/news/news-detail/news-detail.js
 var postsData = require('../../../data/news-data.js');
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    isPlayingMusic: false
   },
 
   /**
@@ -26,11 +27,61 @@ Page({
       this.setData({
         collected: postCollected
       })
-    } else {
+    }
+    else {
       var postsCollected = {};
       postsCollected[postId] = false;
-      wx.setStorageSync('posts_collected', postsCollected)
+      wx.setStorageSync('posts_collected', postsCollected);
     }
+
+    if(app.globalData.g_isPlayingMusic && app.globalData.g_currentMusicPostId === postId) {
+      this.setData({
+        isPlayingMusic: true
+      })
+    }
+    this.setMusicMonitor();
+  },
+
+  setMusicMonitor: function () {
+    //点击播放图标和总控制图标都会触发这个函数
+    var that = this;
+    wx.onBackgroundAudioPlay(function(event) {
+      var pages = getCurrentPages();
+      var currentPage = pages[pages.length-1];
+      if (currentPage.data.currentPostId === that.data.currentPostId) {
+        // 打开多个post-detail页面后，每个页面不会关闭，只会隐藏。通过页面栈拿到到
+        // 当前页面的postid，只处理当前页面的音乐播放。
+        if (app.globalData.g_currentMusicPostId == that.data.currentPostId) {
+          // 播放当前页面音乐才改变图标
+          that.setData({
+            isPlayingMusic: true
+          })
+        }
+         if(app.globalData.g_currentMusicPostId == that.data.currentPostId )
+         app.globalData.g_currentMusicPostId = that.data.currentPostId;
+      }
+      app.globalData.g_isPlayingMusic = true;  
+    });
+    wx.onBackgroundAudioPause(function () {
+      var pages = getCurrentPages();
+      var currentPage = pages[pages.length - 1];
+      if (currentPage.data.currentPostId === that.data.currentPostId) {
+        if (app.globalData.g_currentMusicPostId == that.data.currentPostId) {
+          that.setData({
+            isPlayingMusic: false
+          })
+        }
+      }
+      app.globalData.g_isPlayingMusic = false;
+       app.globalData.g_currentMusicPostId = null;
+    });
+    wx.onBackgroundAudioStop(function () {
+      that.setData({
+        isPlayingMusic: false
+      })
+      app.globalData.g_isPlayingMusic = false;
+      app.globalData.g_currentMusicPostId = null;
+    });
   },
 
   onCollectionTap: function(event) {
@@ -58,7 +109,7 @@ Page({
     })
   },
 
-  getPostsCollectedSyc: function() {
+  getPostsCollectedSyc: function () {
     var postsCollected = wx.getStorageSync('posts_collected');
     var postCollected = postsCollected[this.data.currentPostId];
     postCollected = !postCollected;
@@ -66,18 +117,43 @@ Page({
     this.showToast(postsCollected, postCollected);
   },
 
-  showToast: function(postsCollected, postCollected) {
+  showToast: function (postsCollected, postCollected) {
     var that = this;
     //更新文章是否有缓存值
     wx.setStorageSync('posts_collected', postsCollected);
-    //更新数据绑定变量，从而切换图片
+    //更新数据绑定变量，从而实现切换图片
     that.setData({
-        collected: postCollected
-      }),
+      collected: postCollected
+    }),
       wx.showToast({
-        title: postCollected ? '收藏成功' : '取消收藏',
+        title: postCollected ? "收藏成功" : "取消收藏",
         duration: 1000,
-        icon: 'success'
+        icon: "success"
       })
+  },
+
+  onMusicTap: function (event) {
+    var currentPostId = this.data.currentPostId;
+    var postData = postsData.postList[currentPostId];
+    var isPlayingMusic = this.data.isPlayingMusic;
+    if(isPlayingMusic) {
+      wx.pauseBackgroundAudio();
+      this.setData({
+        isPlayingMusic: false
+      });
+      app.globalData.g_isPlayingMusic = false;
+    }
+    else{
+      wx.playBackgroundAudio({
+        dataUrl: postData.music.url,
+        title: postData.music.title,
+        coverImgUrl: postData.music.coverImg 
+      });
+      this.setData({
+        isPlayingMusic: true
+      });
+      app.globalData.g_currentMusicPostId = this.data.currentPostId;
+      app.globalData.g_isPlayingMusic = true;
+    }
   }
 })
