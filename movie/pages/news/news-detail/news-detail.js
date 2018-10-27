463b5dac9b2a601efb763bbca36a82c6a7210460
vivien <[1870495688@qq.com](mailto:1870495688@qq.com)>
2018-10-27 14:55:26 +0800

    detail-storage

--- movie/pages/news/news-detail/news-detail.js
+++ movie/pages/news/news-detail/news-detail.js
@@ -1,4 +1,5 @@
 // pages/news/news-detail/news-detail.js
+var postsData = require('../../../data/news-data.js');
 Page({
 
   /**
@@ -11,56 +12,72 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(option) {
+    var postId = option.id;
+    this.data.currentPostId = postId;
+    var postData = postsData.postList[postId];
+    this.setData({
+      postData: postData
+    })
 
+    var postsCollected = wx.getStorageSync('posts_collected');
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+      this.setData({
+        collected: postCollected
+      })
+    } else {
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected)
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onCollectionTap: function(event) {
+    this.getPostsCollectedSyc();
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onShareTap: function (event) {
+    var itemList = [
+      "分享给微信好友",
+      "分享给QQ好友",
+      "分享到朋友圈",
+      "分享到微博"
+    ];
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: '#405f80',
+      success: function (res) {
+        //res.cancel 用户是否点击了取消按钮
+        //res.taoIndex 数组元素的序号，从0开始
+        wx.showModal({
+          title: '用户' + itemList[res.tapIndex],
+          content: '用户是否取消？'  + '现在无法实现分享功能',
+        })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  getPostsCollectedSyc: function() {
+    var postsCollected = wx.getStorageSync('posts_collected');
+    var postCollected = postsCollected[this.data.currentPostId];
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected;
+    this.showToast(postsCollected, postCollected);
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showToast: function(postsCollected, postCollected) {
+    var that = this;
+    //更新文章是否有缓存值
+    wx.setStorageSync('posts_collected', postsCollected);
+    //更新数据绑定变量，从而切换图片
+    that.setData({
+        collected: postCollected
+      }),
+      wx.showToast({
+        title: postCollected ? '收藏成功' : '取消收藏',
+        duration: 1000,
+        icon: 'success'
+      })
   }
 })
