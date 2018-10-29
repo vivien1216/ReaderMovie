108006a62e4d0ad9e8cd406018d0efc66fa79cff
vivien <[1870495688@qq.com](mailto:1870495688@qq.com)>
2018-10-29 16:16:54 +0800

    movie-data

--- movie/pages/movie/movie.js
+++ movie/pages/movie/movie.js
@@ -1,22 +1,75 @@
 // pages/movie/movie.js
+var util = require("../../utils/util.js");
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    inTheaters: {},
+    comingSoon: {},
+    top250: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(event) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
+    this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+    this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+    this.getMovieListData(top250Url, "top250", "豆瓣top250");
   },
-  onMoreMovie: function (event) {
+  
+  getMovieListData: function (url,settedKey,categoryTitle) {
+    var that = this;
+    wx.request({
+      url: url,
+      method: "GET",
+      header: {
+        "content-type": "json"
+      },
+      success: function (res) {
+        that.processDoubanData(res.data, settedKey, categoryTitle);
+      },
+      fail: function (error) {
+        console.log(error);
+      }
+    })
+  },
+
+  onMoreMovie: function(event) {
     wx.navigateTo({
       url: 'more-movie/more-movie',
     })
+  },
+
+  processDoubanData: function (moviesDouban, settedKey, categoryTitle) {
+    var movies = [];
+    for(var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if(title.length >= 6) {
+        title = title.substring(0, 6) + "..."
+      }
+      var temp = {
+        title: title,
+        stars: util.convertToStarsArray(subject.rating.stars),
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp);
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      movies: movies
+    };
+    this.setData(readyData);
   }
 })
